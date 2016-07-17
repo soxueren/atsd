d2622b85815054ead52e5b18ade329842d76ddd9
rodionos <sergei.rodionov@axibase.com>
2016-07-18 01:00:14 +0300

    TCP send example

---
+++ api/network/examples/AtsdSendExample.java
@@ -0,0 +1,51 @@
+import java.text.MessageFormat;
+import java.util.*;
+
+public class AtsdSendExample {
+
+    /**
+     * java AtsdSendExample
+     *
+     * @param args ATSD hostname, ATSD network API port (8081)
+     * @return null.
+     */
+    public static void main(String[] args) throws Exception {
+
+        if (args == null || args.length < 2) {
+            throw new IllegalArgumentException("Program arguments are missing or incomplete: {atsd_hostname} {atsd_network_api_port}");
+        }
+
+        log("start: {0}:{1}", args);
+
+        //initialize TCP client, open connection
+        AtsdTcpClient client = new AtsdTcpClient(args[0], Integer.parseInt(args[1]));
+        client.init();
+
+        //send a numeric sample
+        client.sendSeries(new Date(), "sensor-01", "temperature", 24.4d);
+
+        //send a text message
+        {
+            Map<String, String> tags = new LinkedHashMap<String, String>();
+            tags.put("type", "equipment");
+            tags.put("source", "plant");
+            client.sendMessage(new Date(), "sensor-01", tags, "Shutting down");
+        }
+
+        //send a property
+        {
+            Map<String, String> tags = new LinkedHashMap<String, String>();
+            tags.put("precision", "analog");
+            client.sendProperty(new Date(), "sensor-01", "equipment", null, tags);
+        }
+
+        //close the connection
+        client.shutdown();
+    }
+
+
+    private static void log(String message, Object... arguments) {
+        System.out.println(MessageFormat.format(message, arguments));
+    }
+
+}
